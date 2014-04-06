a1650e6804f667501e65fb2edaf5fa4a2de0f05b
ada10lgu <ada10lgu@student.lth.se>
2014-04-06 16:04:15 +0200

    Nu uppdateras UI när man gör nya kakor

--- src/gui/production/storage/StoragePane.java
+++ src/gui/production/storage/StoragePane.java
@@ -2,6 +2,8 @@ package gui.production.storage;
 
 import java.awt.BorderLayout;
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -12,23 +14,36 @@ import model.Factory;
 import model.Pallet;
 
 @SuppressWarnings("serial")
-public class StoragePane extends JPanel {
+public class StoragePane extends JPanel implements Observer {
 
+	JPanel content;
+	Factory f;
+	
 	public StoragePane(Factory f) {
+		this.f = f;
+		f.addObserver(this);
 		setLayout(new BorderLayout());
-		ArrayList<Pallet> pallets = f.getPallets();
 	
 		setBorder(BorderFactory.createTitledBorder("In stock"));
 		JPanel content = new JPanel();
 		content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
-		
-		for (Pallet p : pallets) {
-			content.add(new PalletPane(p));
-		}
+		this.content = content;
+		update(null,null);
 		
 		JScrollPane scroll = new JScrollPane(content);
 		add(scroll,BorderLayout.CENTER);
 		
 	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		content.removeAll();
+		ArrayList<Pallet> pallets = f.getPallets();
+		for (Pallet p : pallets) {
+			content.add(new PalletPane(p));
+		}
+	}
+	
+	
 	
 }
